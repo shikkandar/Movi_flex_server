a23009e7dc9b38165b938a9e46a13dd37415e87f
shikkandar <mohammedshikkandar1@gmail.com>
2024-05-02 23:40:46 +0530

    update 2.0

--- models/Movi.model.js
+++ models/Movi.model.js
@@ -4,6 +4,7 @@ const theaterSchema = new mongoose.Schema({
     name: String,
     moviname:String,
     price:String,
+    rating:Number,
     poster:String,
     description:String,
     runningMovies: Boolean
